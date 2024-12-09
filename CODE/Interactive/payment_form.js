0d158bd95d5a2b48c67f0d932bf5b215c3e053a4
Phal Sovandy <sovandy.phal@student.cadt.edu.kh>
2024-12-09 15:36:08 +0700

    fix all page and add some responsiveness to the landing page

--- CODE/Interactive/payment_form.js
+++ CODE/Interactive/payment_form.js
@@ -1,22 +1,54 @@
 let serviceCard = document.querySelectorAll('.service-card-checkout');
+let rightPay = document.getElementById("info-right");
+let formPay = document.getElementById('check-out-form');
+let cardContainer = document.querySelector('.container-card-service');
+
+
 serviceCard.forEach((card) => {
     card.addEventListener("click", () =>{
         card.classList.toggle("selected-service");
     });
 });
 
+function toggleButton(selector, content) {
+    const button = document.querySelector(`.${selector}`);
+    if (!button.classList.contains('is-toggled')) {
+        // Turn off any previously toggled button
+        turnOffPreviousButton();
+        // Turn this button ON
+        button.classList.add('is-toggled');
+    } else {
+        // Turn this button OFF
+        button.classList.remove('is-toggled');
+    }
 
-let paymentCard = document.querySelectorAll('.payment-card');
-let rightPay = document.getElementById("info-right");
+    if (button.classList.contains('is-toggled')) {
+        contentRightPay(content);
+    } else {
+        contentRightPay('');
+    }
+}
 
-paymentCard.forEach((card) =>{
-    card.addEventListener("click", () =>{
-        card.classList.toggle("selected-method");
-    })
-})
+function turnOffPreviousButton() {
+    const previousButton = document.querySelector('.is-toggled');
+    if (previousButton) {
+        previousButton.classList.remove('is-toggled');
+    }
+}
 
 function contentRightPay(content){
-    rightPay.innerHTML = `
-    <img src="../../Midea/Service_Cards/${content}.jpg">
-    `;
+    if(!content){
+        rightPay.innerHTML = ' ';
+    }else{
+        rightPay.innerHTML = `
+        <img src="../../Midea/Service_Cards/${content}">
+        `;
+    }
+}
+
+function hideForm(){
+    formPay.style.display = 'none';
+}
+function showForm(){
+    formPay.style.display = 'block';
 }
